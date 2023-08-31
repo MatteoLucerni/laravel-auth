0b4debe44fba9cd398e4ba7ef59900d35a21425b
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-31 18:23:16 +0200

    styled card projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -10,7 +10,7 @@
             <li class="my-5">
                 <div class="card bg-light p-5">
                     <div class="card-header rounded border-0 mb-4 d-flex justify-content-between align-content-center ">
-                        <h2 class="m-0">
+                        <h2 class="m-0 d-flex align-items-center">
                             {{ $proj->title }}
                         </h2>
                         @if ($proj->is_public)
@@ -33,17 +33,22 @@
                                     Other Languages: {{ $proj->other_langs }}
                                 </li>
                                 <li>
-                                    Main Language: {{ $proj->main_lang }}
-                                </li>
-                                <li>
                                     Stars: {{ $proj->n_stars }}
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div class="card-footer border-0 bg-light text-end ">
-                        Creazione: {{ $proj->created_at }} <br>
-                        Ultima Modifica: {{ $proj->updated_at }}
+                    <div class="card-footer d-flex justify-content-between mt-3 align-items-center border-0 bg-light">
+                        <div class="buttons">
+                            <a href="#" class="btn btn-success">Click</a>
+                            <a href="#" class="btn btn-success">Click</a>
+                            <a href="#" class="btn btn-success">Click</a>
+                            <a href="#" class="btn btn-success">Click</a>
+                        </div>
+                        <div class="text-end">
+                            Creazione: {{ $proj->created_at }} <br>
+                            Ultima Modifica: {{ $proj->updated_at }}
+                        </div>
                     </div>
                 </div>
             </li>
