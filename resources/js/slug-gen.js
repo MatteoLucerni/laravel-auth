07d3303669a13dcdace32255112876b3e73a2009
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-01 10:47:48 +0200

    added slug generator

---
+++ resources/js/slug-gen.js
@@ -0,0 +1,12 @@
+console.log("Js Loaded");
+
+const slugField = document.getElementById("slug");
+const titleField = document.getElementById("title");
+
+titleField.addEventListener("input", () => {
+    slugField.value = titleField.value
+        .trim()
+        .toLowerCase()
+        .split(" ")
+        .join("-");
+});
