0263ca7a660b7adb66010716ce02edefe069f3d7
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-01 12:11:05 +0200

    added btn go back in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -5,6 +5,9 @@
 @section('content')
     <h1 class="text-center mt-5">Create a new project</h1>
     @include('includes.form')
+    <div class="d-flex justify-content-center my-5">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Go back</a>
+    </div>
 @endsection
 
 @section('scripts')
