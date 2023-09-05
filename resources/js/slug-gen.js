44dbfc761dca003caa17b73d07c71e837603f2b4
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-05 22:04:00 +0200

    added secondary  input value

--- resources/js/slug-gen.js
+++ resources/js/slug-gen.js
@@ -1,10 +1,11 @@
 console.log("Js Loaded");
 
+const slugViewField = document.getElementById("slug-view");
 const slugField = document.getElementById("slug");
 const titleField = document.getElementById("title");
 
 titleField.addEventListener("input", () => {
-    slugField.value = titleField.value
+    slugField.value = slugViewField.value = titleField.value
         .trim()
         .toLowerCase()
         .split(" ")
